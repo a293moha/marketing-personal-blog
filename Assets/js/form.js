7c680a5726078953198348d5eabbc29de2181165
Abdulahi Mohamed <abdulahim002@yahoo.com>
2024-03-28 17:21:23 -0400

    made changes to blog.js

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -6,9 +6,9 @@ document.addEventListener('blogContent', function () {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         const username = document.getElementById('username').value.trim();
-        const email = document.getElementById('username').value.trim();
-        const title = document.getElementById('username').value.trim();
-        const content = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const title = document.getElementById('title').value.trim();
+        const content = document.getElementById('content').value.trim();
 
         if (!username || !email || !title || !content) {
         alert('Try Again.');
@@ -16,10 +16,10 @@ document.addEventListener('blogContent', function () {
         }
 
         alert('Thank You for your submission!');
-        const posts = JSON.parse(localStorage.getItem('posts'));
-        //object to string
+        const posts = JSON.parse(localStorage.getItem('posts')) || [];
+        
         posts.push({username,email,title,content});
         localStorage.setItem('posts', JSON.stringify(posts));
-        window.localStorage.href = 'blog.html';
+        window.location.href = 'blog.html';
     })
 });
