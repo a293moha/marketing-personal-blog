d215945619e4fcf790de7f9ac0d2ee0bc54509dd
Abdulahi Mohamed <abdulahim002@yahoo.com>
2024-03-28 15:27:05 -0400

    added content in js

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,2 +1,12 @@
 //username page
 //save data of HTML into local storage
+
+const username = document.getElementById('username').value.trim();
+const email = document.getElementById('username').value.trim();
+const title = document.getElementById('username').value.trim();
+const content = document.getElementById('username').value.trim();
+
+document.addEventListener('blogContent', function () {
+    const form = document.getElementById('blogForm');
+    event.preventDefault();
+})
