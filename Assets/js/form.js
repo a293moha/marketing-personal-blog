cb6823906799391f31ade570102f9d185a39becb
Abdulahi Mohamed <abdulahim002@yahoo.com>
2024-03-28 15:50:48 -0400

    more changes made to form.js

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,12 +1,24 @@
 //username page
 //save data of HTML into local storage
-
-const username = document.getElementById('username').value.trim();
-const email = document.getElementById('username').value.trim();
-const title = document.getElementById('username').value.trim();
-const content = document.getElementById('username').value.trim();
-
 document.addEventListener('blogContent', function () {
     const form = document.getElementById('blogForm');
-    event.preventDefault();
-})
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('username').value.trim();
+        const title = document.getElementById('username').value.trim();
+        const content = document.getElementById('username').value.trim();
+
+
+        if (!username || email || title ||content) {
+        alert('Try Again.');
+        return;
+        }
+
+        alert('Thank You for your submission!');
+        });
+    });
+
+
+
