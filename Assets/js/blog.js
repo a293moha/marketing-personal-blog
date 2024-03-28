fbc0815f2b04daccd3081da24119c54cf1a2eee1
Abdulahi Mohamed <abdulahim002@yahoo.com>
2024-03-28 13:53:08 -0400

    toggle theme added in blog.js

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,1 +1,8 @@
 //displaying the blog from local storage.
+
+
+//toggle theme.
+const lightDarkButton = document.getElementById;
+lightDarkButton.addEventListener('click', function () {
+    document.body.classList.toggle(dark-theme);
+})
